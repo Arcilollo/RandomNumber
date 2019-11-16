bb4b2aef2f77f354b4ebc4e07de5f1e61b39747e
Arcilollo <44034496+Arcilollo@users.noreply.github.com>
2019-11-17 00:27:35 +0100

    Initial commit

---
+++ app/src/main/java/org/arci/random/MainActivity.java
@@ -0,0 +1,55 @@
+package org.arci.random;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Button;
+import java.util.*;
+
+public class MainActivity extends Activity {
+
+    SeekBar seekbar;
+    TextView textview;
+    TextView result;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Random r = new Random();
+
+        seekbar = (SeekBar)findViewById(R.id.seekBar);
+        textview = (TextView)findViewById(R.id.Counter);
+        result = (TextView)findViewById(R.id.Result);
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View TextView) {
+                result.setText(String.valueOf(r.nextInt(seekbar.getProgress()+1)));
+            }
+        });
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                textview.setText(" " + i);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+}
